d2c5bb3a95bcbf0f9fd0dd713b1350794d22fe40
Rohith Maddali <1015083@student.sae.edu.au>
2019-06-14 17:26:52 +1000

    Fresh new project handle with care

---
+++ GAD170/Assets/Testscript_01.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testscript_01 : MonoBehaviour
+{
+    public int fruit;
+    public int apples;
+    public int var;
+    int playerlives = 50;
+    public int classSize = 25;
+    public bool classCounted;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        /* HelloWorld();
+         modifylives(-2);
+         displayname("Rohith", "Maddali");*/
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(Input.GetKeyDown(KeyCode.Space))
+        {
+            //simple 1d6 dice
+            // if(Random.Range(successCalc,100) > 75)
+            if (Random.Range(1,7) > 4)
+            {
+                HelloWorld();
+                lives(2);
+                gameover(playerlives);
+                Debug.Log("you won");
+            }
+            else
+            {
+                Debug.Log("you lost");
+            }
+            
+        }
+        if (!classCounted)
+        {
+            for (int i = 1; i < classSize; i++)
+            {
+                Debug.Log(i);
+            }
+            classCounted = true;
+        }
+
+
+    }
+
+    void HelloWorld()
+    {
+        Debug.Log("Hello world");
+    }
+
+    void modifylives(int inclives)
+    {
+        playerlives += inclives;
+        Debug.Log(inclives);
+    }
+    void displayname(string firstname, string lastname)
+    {
+        Debug.Log(firstname + " " + lastname);
+    }
+    void lives(int n)
+    {
+        
+            playerlives -= n;
+            Debug.Log(playerlives);
+    }
+    void gameover(int n)
+    {
+        if(n<=0)
+        {
+            Debug.Log("Game Over");
+        }
+        else
+        {
+            Debug.Log("You have " + n + " lives left");
+        }
+        
+    }
+    void addexp()
+    {
+
+    }
+    void levelup()
+    {
+
+    }
+    void attributes()
+    {
+
+    }
+     
+
+
+
+
+
+}
